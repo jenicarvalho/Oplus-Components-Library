7edbcf6958976c77910fcd4054236235fc5e8534
Jeniffer Carvalho <jenicarvalho@live.com>
2019-12-17 21:17:18 -0300

    create button style

---
+++ src/components/atoms/Button/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  color: #fff;
+  border-radius: 3px;
+  border: none;
+  outline: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+`;
