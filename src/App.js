e8b58d6d1a481b9d183a773f73574b163c9e9e30
Jeniffer Carvalho <jenicarvalho@live.com>
2019-12-25 19:05:07 -0300

    add components on app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Avatar from "./components/atoms/Avatar/Avatar";
+import Card from "./components/atoms/Card/Card";
+import Button from "./components/atoms/Button/Button";
+import Badge from "./components/atoms/Badge/Badge";
+import Logo from "./components/atoms/Logo/Logo";
+import Notification from "./components/atoms/Notification/Notification";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Avatar />
+      <Card />
+      <Button />
+      <Badge />
+      <Logo />
+      <Notification />
     </div>
   );
 }
