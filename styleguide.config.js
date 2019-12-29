dcc6f4678b2c5d867047d9be3f66c3bcb2d4305c
Jeniffer Carvalho <jenicarvalho@live.com>
2019-12-29 20:31:02 -0300

    add sections on sidebar

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,27 @@
 module.exports = {
-  ignore: ["**/**/**/styles.js", "**/**/*.test.js"]
+  ignore: ["**/**/**/styles.js", "**/**/*.test.js"],
+  title: "Oplus Components",
+  pagePerSection: true,
+  sections: [
+    {
+      name: "Components",
+      sectionDepth: 2,
+      sections: [
+        {
+          name: "Atoms",
+          components: () => ["./src/components/atoms/**/*.js"],
+          sectionDepth: 2,
+          exampleMode: "expand",
+          usageMode: "expand"
+        },
+        {
+          name: "Molecules",
+          components: () => ["./src/components/molecules/**/*.js"],
+          sectionDepth: 2,
+          exampleMode: "expand",
+          usageMode: "expand"
+        }
+      ]
+    }
+  ]
 };
