8826795793d623d7549bffc25a84143e03624781
Jeniffer Carvalho <jenicarvalho@live.com>
2019-12-29 20:51:07 -0300

    create styles  NavBar

---
+++ src/components/molecules/NavBar/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Nav = styled.div`
+  background-color: #ff7d01;
+  padding: 10px 15px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  width: 100%;
+`;
