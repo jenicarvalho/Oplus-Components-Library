9f3298cc4f9670164069d0431302d3e3b020f075
Jeniffer Carvalho <jenicarvalho@live.com>
2020-01-14 15:40:02 -0300

    create custom tests for badge

--- src/components/atoms/Badge/Badge.test.js
+++ src/components/atoms/Badge/Badge.test.js
@@ -6,4 +6,10 @@ describe("Is the Badge component working?", () => {
   it("Is it rendering?", () => {
     TestRenderer.create(<Badge />);
   });
+  it("It is rendering with custom text?", () => {
+    TestRenderer.create(<Badge badgeText="Custom text" />);
+  });
+  it("Is it rendering with custom color and textColor?", () => {
+    TestRenderer.create(<Badge badgeColor="red" badgeTextColor="#ddd" />);
+  });
 });
