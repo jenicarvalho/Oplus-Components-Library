08c6c90d80637104eb43c8885843deb40855514a
Jeniffer Carvalho <jenicarvalho@live.com>
2020-01-14 15:41:00 -0300

    create custom test for Logo

--- src/components/atoms/Logo/Logo.test.js
+++ src/components/atoms/Logo/Logo.test.js
@@ -6,4 +6,7 @@ describe("Is the Logo component working?", () => {
   it("Is it rendering?", () => {
     TestRenderer.create(<Logo />);
   });
+  it("Is it rendering in white version?", () => {
+    TestRenderer.create(<Logo white />);
+  });
 });
