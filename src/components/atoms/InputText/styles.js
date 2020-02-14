79e33a2c7369171d9ec16f767d00525fe42e883c
Jeniffer Carvalho <jenicarvalho@live.com>
2020-02-13 21:03:36 -0300

    Created style InputText

---
+++ src/components/atoms/InputText/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  input {
+    border: 1px solid #ddd;
+    padding: 8px 10px;
+
+    &:focus {
+      outline: none;
+      border-color: #aaa
+    }
+  }
+`;
