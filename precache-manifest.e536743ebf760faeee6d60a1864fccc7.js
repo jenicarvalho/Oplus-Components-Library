e07d668d479f969698237ec297cf25ee218930d0
Jeniffer Carvalho <j.francyelly-sys@parceiros-kroton.com.br>
2020-02-18 17:16:59 -0300

    Updates

---
+++ precache-manifest.e536743ebf760faeee6d60a1864fccc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03dbd6b8f80b217a1f86d269549a0eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e92d802c32f11c97cbe9",
+    "url": "/static/js/2.cdc5f32f.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.cdc5f32f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "887cd08675c0ff193dff",
+    "url": "/static/js/main.bf063799.chunk.js"
+  },
+  {
+    "revision": "17edd0779face5e0acfb",
+    "url": "/static/js/runtime-main.88e1bb28.js"
+  }
+]);
